c51352c8ea759edd9d0976ddaf6d03cbcb08257b
Era Dorta <e.dorta@tudelft.nl>
2024-03-21 11:46:05 +0100

    Add instructions and files for serving the continuous annotation task

--- samples/continuous_annotation/covfee.deployment.config.py
+++ samples/continuous_annotation/covfee.deployment.config.py
@@ -3,9 +3,3 @@ BASE_URL = "https://covfee.ewi.tudelft.nl/covfee"
 COVFEE_SECRET_KEY = "MY_SECRET"
 ADMIN_USERNAME = "admin2"
 ADMIN_PASSWORD = "admin2"
-
-# Deploy without socket
-# gunicorn --worker-class eventlet -w 4 'covfee.server.app:create_app()' --bind 0.0.0.0:5002
-
-# Deploy with socket
-# gunicorn --worker-class eventlet -w 1 --bind unix:covfee.sock -m 007 'covfee.server.app:create_app()'
