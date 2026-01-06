5c0c8a6e4e53b1bedf425b66b6f1477d59078a34
agmercier <a.g.mercier@gmail.com>
2026-01-06 15:00:55 +0100

    duplicated front end task folder so changes for the ingroup annotation task can be made here

---
+++ covfee/client/tasks/ingroup_annotation/spec.ts
@@ -0,0 +1,31 @@
+import { BaseTaskSpec } from "@covfee-shared/spec/task"
+/**
+ * @TJS-additionalProperties false
+ */
+export interface AnnotationDataSpec {
+  category: string
+  participant: string
+  interface: "RankTrace" | "GTrace" | "Binary"
+}
+export interface MediaSpec {
+  type: "video/mp4"
+  src: string
+}
+
+export interface ContinuousAnnotationTaskSpec extends BaseTaskSpec {
+  /**
+   * @default "ContinuousAnnotationTask"
+   */
+  type: "ContinuousAnnotationTask"
+  media: MediaSpec[]
+  annotations: AnnotationDataSpec[]
+  prolificCompletionCode?: string
+  taskVariantPopupBulletPoints?: string[]
+  userCanAdd: boolean
+  /**
+   * When specified: True, means audio on is mandatory, False means audio off (muted) is mandatory.
+   * @default null
+   */
+  audioRequirement?: boolean
+  videoTutorialUrl?: string
+}
